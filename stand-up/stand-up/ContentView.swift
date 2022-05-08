40ee79f2f8855d0d10695c19a2f46ba75a97bf0f
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-05-08 14:34:19 +0900

    프로젝트 생성

---
+++ stand-up/stand-up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stand-up
+//
+//  Created by Hyeongjung on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stand-up/stand-up/stand_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  stand_upApp.swift
+//  stand-up
+//
+//  Created by Hyeongjung on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct stand_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
